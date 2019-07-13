e7fcd1e8a50549925593c1be0f3bf1484c98173c
Ignacio Gaxiola <ignacioggb@hotmail.com>
2019-07-13 00:27:39 -0700

    created react app

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="bottom">
+    Copyright  {'\u00A9'}
+    </div>
+  </footer>
+);
+
+export default Footer;
